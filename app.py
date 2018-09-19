b39aa3d373b17ff311b9fcede27b0d5c2dbb92cd
bowbowbow <clsrn1581@gmail.com>
2018-09-19 21:33:04 +0900

    Update config path

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ sys.path.append(base_dir)
 
 app = flask.Flask(__name__)
 
-app.config.from_pyfile('config.cfg')
+app.config.from_object('config.Config')
 db = MongoEngine(app)
 
 app.add_url_rule('/', view_func=views.index)
 app.add_url_rule('/pagination', view_func=views.pagination)
 
 if __name__ == '__main__':
-    FLASK_DEBUG = os.getenv('FLASK_DEBUG', False)
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG', True)
     app.run(host='0.0.0.0', debug=FLASK_DEBUG, port=8080)
