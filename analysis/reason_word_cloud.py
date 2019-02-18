197aac3c8bce7b4671e2b60b5a0124b5c60e144a
bowbowbow <clsrn1581@gmail.com>
2019-02-18 17:08:19 +0900

    remove definition words of attribute

--- analysis/reason_word_cloud.py
+++ analysis/reason_word_cloud.py
@@ -5,19 +5,44 @@ from mongoengine.queryset.visitor import Q
 from tqdm import tqdm
 from collections import defaultdict
 
+from nltk import ngrams, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
+
 from models import Doc, User, Sent, Annotation
 import config
 
 
-def clean_text(x):
-    x = str(x)
+def clean_reason(reason):
+    reason = reason.lower()
+    reason = reason.replace("'d", ' had')
+    reason = reason.replace("n't", ' not')
+
+    for word in ['could', 'would', 'the', "'s"]:
+        reason = reason.replace(word, '')
     for punct in "/-'":
-        x = x.replace(punct, ' ')
+        reason = reason.replace(punct, ' ')
     for punct in '&':
-        x = x.replace(punct, ' {} '.format(punct))
+        reason = reason.replace(punct, ' {} '.format(punct))
     for punct in '?!.,"#$%\'()*+-/:;<=>@[\\]^_`{|}~' + '“”’':
-        x = x.replace(punct, '')
-    return x
+        reason = reason.replace(punct, '')
+    return reason
+
+
+def get_attribute_words(attribute_key, attribute_value):
+    tokens = word_tokenize(attribute_key.replace('_', ' '))
+    tokens += word_tokenize(attribute_value.replace('_', ' '))
+    result = []
+    for token in tokens:
+        if token == 'not':
+            continue
+        result.append(token.lower())
+    return result
 
 
 def draw_word_cloud():
@@ -48,15 +73,6 @@ def draw_word_cloud():
 
     random.shuffle(dumps)
 
-    from nltk import ngrams, word_tokenize
-    from nltk.corpus import stopwords
-    from nltk.stem.snowball import SnowballStemmer
-    from nltk.stem import WordNetLemmatizer
-
-    stop_words = set(stopwords.words('english'))
-    lemmatizer = WordNetLemmatizer()
-    stemmer = SnowballStemmer("english", ignore_stopwords=True)
-
     def get_ngrams(tokens, n):
         ngrams_ = ngrams(tokens, n)
         return [' '.join(grams) for grams in ngrams_]
@@ -75,25 +91,20 @@ def draw_word_cloud():
             if not reason:
                 continue
 
-            reason = reason.lower()
-            reason = reason.replace("'s", '')
-            reason = reason.replace("'d", ' had')
-            reason = reason.replace("n't", ' not')
-            reason = reason.replace('.', '')
-            reason = reason.replace(',', '')
-            reason = reason.replace('the', '')
-            reason = reason.replace('would', '')
-            reason = reason.replace('could', '')
-
-            reason = clean_text(reason)
+            attribute_words = get_attribute_words(attribute_key, value)
+            reason = clean_reason(reason)
 
             tokens = word_tokenize(reason)
             clean_tokens = []
             for token in tokens:
-                if token not in stop_words or token == 'not':
-                    # token = stemmer.stem(token)
-                    token = lemmatizer.lemmatize(token, pos='v')
-                    clean_tokens.append(token)
+                if token in stop_words and token != 'not':
+                    continue
+                if token in attribute_words and token != 'not':
+                    continue
+
+                # token = stemmer.stem(token)
+                token = lemmatizer.lemmatize(token, pos='v')
+                clean_tokens.append(token)
 
             # attribute_reason[attribute_key][value] += get_ngrams(clean_tokens, 2)
             attribute_reason[attribute_key][value] += get_ngrams(clean_tokens, 3)
@@ -107,11 +118,11 @@ def draw_word_cloud():
     import matplotlib.pyplot as plt
     from wordcloud import WordCloud
 
-    # attribute_key = 'Perceived_Author_Credibility'
     # attribute_key = 'Knowledge_Awareness'
-    # attribute_key = 'Verifiability'
+    attribute_key = 'Verifiability'
     # attribute_key = 'Disputability'
-    attribute_key = 'Acceptance'
+    # attribute_key = 'Perceived_Author_Credibility'
+    # attribute_key = 'Acceptance'
     max_words = 150
 
     for option in attribute_reason[attribute_key]:
@@ -131,7 +142,7 @@ def draw_word_cloud():
             max_words=max_words,
         ).generate_from_frequencies(target)
 
-        plt.figure(figsize=(20, 20))
+        plt.figure(figsize=(25, 25))
         plt.imshow(wordcloud)
         plt.axis('off')
         plt.savefig('./plt/{}-{}'.format(attribute_key, option))
