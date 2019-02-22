cbc7962d176d6ae847c9c627c6a4de82077c5bdb
bowbowbow <clsrn1581@gmail.com>
2019-02-23 03:12:40 +0900

    add bar chart

--- analysis/change_chart.py
+++ analysis/change_chart.py
@@ -8,7 +8,7 @@ from models import Doc, User, Sent, Annotation
 import config
 
 
-def draw_daily_amount_chart():
+def get_annotations():
     annotations = Annotation.objects(type='sentence')
 
     dumps = []
@@ -29,32 +29,55 @@ def draw_daily_amount_chart():
         pickle.dump(dumps, open(bin_path, "wb"))
 
     random.shuffle(dumps)
+    return dumps
 
+
+def draw_daily_amount_chart(annotations):
     day_count = defaultdict(lambda: 0)
     import dateutil.parser
-    for annotation in tqdm(dumps):
+    from datetime import datetime, timedelta
+
+    for annotation in tqdm(annotations):
         date = dateutil.parser.parse(annotation['created_at'])
-        date_str = '{}-{}-{}'.format(date.year, date.month, date.day)
-        day_count[date_str] += 1
+        day_count[date.strftime('%y-%m-%d')] += 1
 
-    xy = day_count.items()
+    x = []
+    y = []
+    date = datetime.strptime('18-10-10', '%y-%m-%d')
+    while date < datetime.now():
+        date_key = date.strftime('%y-%m-%d')
+        x.append(date_key)
+        s = day_count[date_key]
+        if len(y):
+            s += y[-1]
+        y.append(s)
+        date += timedelta(days=1)
 
-    def compare(item):
-        s = item[0].split('-')
-        s = [int(si) for si in s]
-        return s[0] * 365 + s[1] * 31 + s[2]
+    def sampling(x, y):
+        rs = []
+        ry = []
+        for i in range(0, len(x)):
+            if i % 8 == 0:
+                rs.append(x[i])
+                ry.append(y[i])
+        return rs, ry
 
-    xy = sorted(xy, key=compare)
-    x = [item[0] for item in xy]
-    y = [item[1] for item in xy]
-    sy = [0] * len(x)
-    sy[0] = y[0]
-    for i in range(1, len(sy)):
-        sy[i] = y[i] + sy[i - 1]
+    x, y = sampling(x, y)
 
-    print('sy :', sy)
+    # print(list(zip(x, y)))
+
+    from matplotlib import pyplot as plt
+    fig, ax = plt.subplots()
+
+    fig.autofmt_xdate()
+    plt.bar(x, y)
+
+    plt.grid(b=True, which='major', color='#999999', linestyle='-', alpha=0.2)
+
+    plt.show()
 
 
 if __name__ == '__main__':
     connect(**config.Config.MONGODB_SETTINGS)
-draw_daily_amount_chart()
+    annotations = get_annotations()
+    draw_daily_amount_chart(annotations)
