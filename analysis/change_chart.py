977587066250d8a7a71334d23a58ae8b3459704f
bowbowbow <clsrn1581@gmail.com>
2019-02-24 04:50:38 +0900

    add clustering using glove

--- analysis/change_chart.py
+++ analysis/change_chart.py
@@ -23,7 +23,9 @@ def get_annotations():
                     continue
                 if not ('value' in annotation['basket']['Acceptance']):
                     continue
-                dumps.append(annotation.dump())
+                dump = annotation.dump()
+                dump['is_turk'] = not annotation.user.is_active
+                dumps.append(dump)
             except Exception as e:
                 logging.exception(e)
         pickle.dump(dumps, open(bin_path, "wb"))
@@ -32,37 +34,56 @@ def get_annotations():
     return dumps
 
 
-def draw_daily_amount_chart(annotations):
-    day_count = defaultdict(lambda: 0)
+def draw_daily_amount_chart_with_turk(annotations):
+    day_student_count = defaultdict(lambda: 0)
+    day_turk_count = defaultdict(lambda: 0)
     import dateutil.parser
     from datetime import datetime, timedelta
 
     for annotation in tqdm(annotations):
         date = dateutil.parser.parse(annotation['created_at'])
-        day_count[date.strftime('%y-%m-%d')] += 1
+        date_key = date.strftime('%y-%m-%d')
+        if annotation['is_turk']:
+            day_turk_count[date_key] += 1
+        else:
+            day_student_count[date_key] += 1
 
     x = []
-    y = []
+    sy = []
+    ty = []
     date = datetime.strptime('18-10-10', '%y-%m-%d')
     while date < datetime.now():
         date_key = date.strftime('%y-%m-%d')
         x.append(date_key)
-        s = day_count[date_key]
-        if len(y):
-            s += y[-1]
-        y.append(s)
+        item = day_student_count[date_key]
+        if len(sy):
+            item += sy[-1]
+        sy.append(item)
+        item = day_turk_count[date_key]
+        if len(ty):
+            item += ty[-1]
+        ty.append(item)
         date += timedelta(days=1)
 
-    def sampling(x, y):
-        rs = []
-        ry = []
+    import pandas as pd
+    df = pd.DataFrame(columns=['date', 'student', 'turker', 'sum'])
+    for i in range(len(x)):
+        df.loc[i] = [x[i], sy[i], ty[i], sy[i]+ty[i]]
+
+    writer = pd.ExcelWriter('./chart/number_of_annotations.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+    def sampling(x):
+        res = []
         for i in range(0, len(x)):
             if i % 8 == 0:
-                rs.append(x[i])
-                ry.append(y[i])
-        return rs, ry
+                res.append(x[i])
+        return res
 
-    x, y = sampling(x, y)
+    x = sampling(x)
+    ty = sampling(ty)
+    sy = sampling(sy)
 
     # print(list(zip(x, y)))
 
@@ -70,14 +91,20 @@ def draw_daily_amount_chart(annotations):
     fig, ax = plt.subplots()
 
     fig.autofmt_xdate()
-    plt.bar(x, y)
+    plt.bar(x, sy, label='Student')
+    plt.bar(x, ty, bottom=sy, label='Turker')
 
     plt.grid(b=True, which='major', color='#999999', linestyle='-', alpha=0.2)
+    plt.legend(loc='upper left')
 
     plt.show()
+    fig.savefig('./chart/number_of_annotations2.png')
+
+
+
 
 
 if __name__ == '__main__':
     connect(**config.Config.MONGODB_SETTINGS)
     annotations = get_annotations()
-    draw_daily_amount_chart(annotations)
+    draw_daily_amount_chart_with_turk(annotations)
