4242339351db44873610729cf3b0d53be6fea925
bowbowbow <clsrn1581@gmail.com>
2019-03-27 12:09:47 +0900

    add annotation manager class

---
+++ analysis/hierachy_v2.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    from analysis.data_util import Annotation
+    from analysis.entailment import Predictor
+
+    anno = Annotation()
+    reasons = anno.get_reasons(anno.acceptance, anno.strong_accept)
+
+    predictor = Predictor()
+
+    with open('./data/entailment/{}_{}.txt'.format(anno.acceptance, anno.strong_accept), 'w+') as f:
+        for reason in reasons:
+            f.write('Text : {}\n'.format(reason))
+            f.write('Hypothesis: {}\n'.format(reason[0]))
+            result = predictor.predict(premise=reason, hypothesis=reason[0])
+            f.write('Prediction: {}/{}/{}\n\n'.format(result[0], result[1], result[2]))
