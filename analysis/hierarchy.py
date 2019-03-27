69381513b8d5e760f5e06b71c27e2c343dbc4ab5
bowbowbow <clsrn1581@gmail.com>
2019-03-27 15:50:51 +0900

    add point annotation on dendrogram

--- analysis/hierarchy.py
+++ analysis/hierarchy.py
@@ -18,6 +18,7 @@ stemmer = SnowballStemmer("english", ignore_stopwords=True)
 
 
 def clustering(reasons, w2v, file_key):
+    print('[clustering] {}'.format(file_key))
     from scipy.cluster import hierarchy
     from analysis.data_util import clean_text, tokenize_and_lemmatize
 
@@ -33,17 +34,18 @@ def clustering(reasons, w2v, file_key):
     dist = 1 - cosine_similarity(X)
 
     # linkage_matrix = hierarchy.ward(dist)  # define the linkage_matrix using ward clustering pre-computed distances
-    linkage_matrix = hierarchy.linkage(X, 'ward')
+    linkage = hierarchy.linkage(dist, 'ward')
+
+    rootnode, nodelist = hierarchy.to_tree(linkage, rd=True)
+    assignments = hierarchy.fcluster(linkage, 4, 'distance')
 
     from matplotlib.pyplot import cm
     cmap = cm.rainbow(np.linspace(0, 1, 5))
     hierarchy.set_link_color_palette([mpl.colors.rgb2hex(rgb[:3]) for rgb in cmap])
 
-    # rootnode, nodelist = hierarchy.to_tree(linkage_matrix)
-
-    # hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
-    fig, ax = plt.subplots(figsize=(20, 30))  # set size
-    ax = hierarchy.dendrogram(linkage_matrix, orientation="right", labels=reasons)
+    from scipy.cluster.hierarchy import fcluster
+    fig, ax = plt.subplots(figsize=(20, len(reasons) * 0.3))
+    ddata = hierarchy.dendrogram(linkage, orientation="right", labels=reasons)
 
     plt.tick_params(
         axis='x',
@@ -52,6 +54,16 @@ def clustering(reasons, w2v, file_key):
         top='off',
         labelbottom='off')
     plt.tight_layout()
+
+    for i, d in zip(ddata['icoord'], ddata['dcoord']):
+        x = 0.5 * sum(i[1:3])
+        y = d[1]
+        plt.plot(x, y, 'ro')
+        plt.annotate("%.3g" % y,
+                     (x, y),
+                     xytext=(0, -8),
+                     textcoords='offset points',
+                     va='top', ha='center')
 
     plt.savefig('./data/dendrogram/{}.png'.format(file_key), dpi=200)
     plt.close()
@@ -65,5 +77,4 @@ if __name__ == '__main__':
     reasons = anno.get_reasons(anno.acceptance, anno.strong_accept)
 
     w2v = load_glove()
-
     clustering(reasons[:100], w2v, '{} {}'.format(anno.acceptance, anno.strong_accept))
